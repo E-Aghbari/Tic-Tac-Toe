281a6d85a0455ae81bb2a8129deacb0cd44d11e9
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2023-06-03 02:45:22 +0100

    Game functions

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,31 +3,85 @@ class Game:
 
     players = []
 
-    start = [0 for i in range(9)]
-
-    grids = {"Top left": start[0], "Top": start[1], "Top right": start[2], "Mid left": start[3], "Mid": start[4], "Mid right": start[5], "Bottom left": start[6] , "Bottom": start[7], "Bottom right": start[8]}
-
-    row = 3
-    column = 3
+    grids = {"Top left": " ", "Top": " ", "Top right": " ", "Mid left": " ", "Mid": " ", "Mid right": " ", "Bottom left": " " , "Bottom": " ", "Bottom right": " "}
 
     rounds = 0
 
-    def print_board(self, symbol = None):
+    def print_board(self):
+        
 
-        for i in range(12):
-            print("        |               |    ")
-            if i == 3 or i== 7:
-                print(" ------------------------------")
+        print( "           |           |         ")
+        print("     {}     |     {}     |     {}    ".format(Game.grids["Top left"], Game.grids["Top"], Game.grids["Top right"]))
+        print( "           |           |         ")
+        print("---------------------------------")
+        print( "           |           |         ")
+        print("     {}     |     {}     |     {}    ".format(Game.grids["Mid left"], Game.grids["Mid"], Game.grids["Mid right"]))
+        print( "           |           |         ")
+        print("----------------------------------")
+        print( "           |           |         ")
+        print("     {}     |     {}     |     {}    ".format(Game.grids["Bottom left"], Game.grids["Bottom"], Game.grids["Bottom right"]))
+        print( "           |           |         ")
+        
+        game.check_win(player1, player2)
     
     def add_players(self):
         if type(self) is Player:
             Game.players.append(self)
 
-    def check_win():
-        print(Game.start)
-        if sum(Game.start[:3]) or sum(Game.start[3:6]) or sum(Game.start[6:]) or (Game.start[0] + Game.start[4] + Game.start[8]) or (Game.start[2] + Game.start[4] + Game.start[6]) == 3:
+    def check_win(self, player1, player2):
+        if Game.grids["Top left"] == Game.grids["Top"] == Game.grids["Top right"] == player1.symbol:
+            print(f"{player1.name} has won")
             Game.game_over = True
-            print(f"Game Over")
+        elif Game.grids["Mid left"] == Game.grids["Mid"] == Game.grids["Mid right"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Bottom left"] == Game.grids["Bottom"] == Game.grids["Bottom right"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] == Game.grids["Mid left"] == Game.grids["Bottom left"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top"] == Game.grids["Mid"] == Game.grids["Bottom"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top right"] == Game.grids["Mid right"] == Game.grids["Bottom right"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] == Game.grids["Mid"] == Game.grids["Bottom right"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top right"] == Game.grids["Mid"] == Game.grids["Bottom left"] == player1.symbol:
+            print(f"{player1.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] == Game.grids["Top"] == Game.grids["Top right"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Mid left"] == Game.grids["Mid"] == Game.grids["Mid right"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Bottom left"] == Game.grids["Bottom"] == Game.grids["Bottom right"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] == Game.grids["Mid left"] == Game.grids["Bottom left"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top"] == Game.grids["Mid"] == Game.grids["Bottom"] == player2.symbol: 
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top right"] == Game.grids["Mid right"] == Game.grids["Bottom right"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] == Game.grids["Mid"] == Game.grids["Bottom right"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top right"] == Game.grids["Mid"] == Game.grids["Bottom left"] == player2.symbol:
+            print(f"{player2.name} has won")
+            Game.game_over = True
+        elif Game.grids["Top left"] != " " and Game.grids["Top"] != " " and Game.grids["Top right"] != " " and Game.grids["Mid left"] != " " and Game.grids["Mid"] != " " and Game.grids["Mid right"] != " " and Game.grids["Bottom left"] != " " and Game.grids["Bottom"] != " " and Game.grids["Bottom right"] != " ":
+            print("It's a tie")
+            Game.game_over = True
+
+
 
     def new_rounds(self):
         Game.rounds += 1
@@ -35,7 +89,7 @@ class Game:
 
     def __repr__(self) -> str:
         return f"This game called Tic Tac Toe its comprised of {len(Game.grids)} grids and {len(Game.players)} players. First player fills three grids in one row, wins."
-
+ 
 
 class Player:
     def __init__(self, name, symbol) -> None:
@@ -44,22 +98,16 @@ class Player:
         Game.add_players(self)
     
     def choose_grid(self, grid):
-        print(Game.start)
-        print(Game.grids)
-        if Game.grids[grid] == 1:
-            return "This grid is already taken."
-        if grid in Game.grids.keys():
-            Game.start[0] = 1
+        if Game.grids[grid] != " ":
+            grid = input( "Nah bro. Choose another one\n")
+            self.choose_grid(grid)
+
+        if Game.grids[grid] == " ":
             self.print_move(grid)
 
-
-            
-
     def print_move(self, grid):
-        pass
-
-    def take_turn(self):
-        pass
+        Game.grids[grid] = self.symbol
+        
 
     def __repr__(self) -> str:
         return f"{self.name}"
@@ -70,11 +118,13 @@ player2 = Player("Abdullah", "O")
 game = Game()
 game.print_board()
 
-while not Game.game_over:
-    chosen_grid = input(f"Choose a grid {player1.name}")
+while not game.game_over:
+    chosen_grid = input(f"Choose a grid {player1.name}\n")
     player1.choose_grid(chosen_grid)
-    chosen_grid = input(f"Choose a grid {player2.name}")
+    game.print_board()
+    chosen_grid = input(f"Choose a grid {player2.name}\n")
     player2.choose_grid(chosen_grid) 
+    game.print_board()
 
     
-    Game.check_win()
+    
